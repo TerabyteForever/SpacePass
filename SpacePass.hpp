d93ceac34797ace534ede61b5a3f654682b74af7
Ali Sancakli <54219476+TerabyteForever@users.noreply.github.com>
2020-08-20 02:26:43 +0300

    Added Space64 support

--- SpacePass.hpp
+++ SpacePass.hpp
@@ -22,7 +22,7 @@ using namespace std;
 
 namespace SpacePass{
     char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','W','X','Y','Z'};
-    void BasicEncryption(char plaintext[], bool writeToFile){ //Basic level encryption. That method scans your string letter-by-letter and 
+    void BasicEncryption(char plaintext[], bool writeToFile){  
 
             
             
@@ -106,6 +106,66 @@ namespace SpacePass{
 
 
     }
+    void EncryptSpace64(char text[], bool writeToFile, bool isDebugMode){
+    	
+
+    vector<char> output;
+    string tempStr;
+    vector<string> tempVec;
+    int tempDec;
+    for(int i = 0; i < strlen(text); i++){
+
+
+            tempStr = SpaceBits::ConvertDecimalToBinAsStr(text[i]);
+            if(tempStr.size() > 0){
+
+            tempStr.erase(6);
+            tempVec.push_back(tempStr);
+
+            }
+
+
+
+
+
+    }
+    for(int i = 0; i < tempVec.size(); i++){
+
+
+        tempDec = SpaceBits::ConvertBinToDec(tempVec.at(i));
+       	output.push_back(tempDec); 
+
+
+    }
+    cout<<"The original text is : "<<text<<endl<<"The Space64 encrypted text is : ";
+    for(int i = 0; i < output.size(); i++){
+    	
+    	
+    	cout<<output.at(i);
+    	
+    	
+    	
+	}
+
+	if(writeToFile == true){
+		
+		
+		ofstream of("Space64Output.txt");
+		for(int i = 0; i < output.size(); i++){
+			
+			of<<output.at(i);
+			
+			
+		}
+		
+		of.eof();
+		
+	}
+    	
+    	
+    	
+    	
+	}
 
 
 
