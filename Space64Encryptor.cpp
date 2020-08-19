e6b431ee6b5acea8bc870c5ef7ab4a2304701190
Ali Sancakli <54219476+TerabyteForever@users.noreply.github.com>
2020-08-20 02:28:56 +0300

    Space64 example

---
+++ Space64Encryptor.cpp
@@ -0,0 +1,18 @@
+#include "SpacePass.hpp"
+
+int main(){
+	
+	
+	
+char text[] = "youcanmakemecustomizablealso";
+	
+	
+	SpacePass::EncryptSpace64(text,false,false);
+	
+	
+	
+	
+	
+	
+	return 0;
+}
